7475d03489eccd765e9d54a0a9eb7a217acd395b
k03302 <unisonman660@gmail.com>
2024-11-07 19:37:47 +0900

    Update Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,9 @@
 "use client"
 
 import { useRouter } from "next/navigation"
-import { useEffect } from "react";
+import { useEffect, useState, useCallback, FormEvent } from "react";
+import { useAuthStore } from '@/stores/useAuthStore';
+
 
 export default function Navbar() {
     // useEffect(() => {
@@ -12,12 +14,36 @@ export default function Navbar() {
     // }, []);
 
     const router = useRouter();
+    const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
+    const setIsLoggedIn = useAuthStore((state) => state.setIsLoggedIn);
+
+    const handleLogin = useCallback(() => {
+        router.push('/login');
+    }, []);
+
+    const handleLogout = useCallback(() => {
+        setIsLoggedIn(false);
+    }, []);
+
+    const handleEnterFavorites = useCallback(() => {
+        if (isLoggedIn) {
+            router.push("/favorites");
+        }
+        else {
+            router.push("/login");
+        }
+    }, [isLoggedIn])
 
     return (
         <div className="navbar">
             <img src="" alt="go back" onClick={() => { router.back(); }} />
-            <p onClick={() => { router.push("/"); }}>MyRecipe</p>
-            <p onClick={() => { router.push("/login"); }}>Login</p>
-        </div>
+            <p onClick={handleEnterFavorites}>즐겨찾기</p>
+            {
+                isLoggedIn ?
+                    <p onClick={handleLogout}>Logout</p>
+                    :
+                    <p onClick={handleLogin}>Login</p>
+            }
+        </div >
     );
 }
